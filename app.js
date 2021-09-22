750867b2362346433d1defeb7ac306d82ce1a826
popka <magemaster555@gmail.com>
2021-09-22 19:12:15 +0300

    Delete trash #2

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const API = require("./core/api")
-const fs = require('fs')
 const login = require("./login.json")
-const session = require("./core/session")
 
 async function start(){
     const api = new API()
     await api.init()
     await api.auth(login.login, login.password, login.cookie)
+    
     const headers = await api.getPage()
     headers.querySelectorAll("h4").forEach(header => {
         console.log(header.text.trim())
